41d34856d7d1ff049bea0c76f5435dc6ce8ef3b8
antocecere77 <antocecere77@gmail.com>
2021-08-05 06:50:20 +0200

    scheduled cache evict

--- src/main/java/com/antocecere77/redisspring/fib/service/FibService.java
+++ src/main/java/com/antocecere77/redisspring/fib/service/FibService.java
@@ -2,6 +2,7 @@ package com.antocecere77.redisspring.fib.service;
 
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +18,12 @@ public class FibService {
     @CacheEvict(value = "math:fib", key = "#index")
     public void clearCache(int index) {
         System.out.println("Clearing hash key");
+    }
+
+    @Scheduled(fixedRate = 10_000)
+    @CacheEvict(value = "math:fib", allEntries = true)
+    public void clearCache() {
+        System.out.println("clearing all fib keys");
     }
 
     //intentional 2^N
