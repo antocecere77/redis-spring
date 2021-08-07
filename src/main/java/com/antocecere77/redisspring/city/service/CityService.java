3c06a342d8b98753e859de9c678aaf6d5701c26d
antocecere77 <antocecere77@gmail.com>
2021-08-07 10:56:26 +0200

    get all city

--- src/main/java/com/antocecere77/redisspring/city/service/CityService.java
+++ src/main/java/com/antocecere77/redisspring/city/service/CityService.java
@@ -7,10 +7,13 @@ import org.redisson.api.RMapReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.codec.TypedJsonJacksonCodec;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class CityService {
@@ -18,10 +21,26 @@ public class CityService {
     @Autowired
     private CityClient cityClient;
 
-    private RMapCacheReactive<String, City> cityMap;
+//    private RMapCacheReactive<String, City> cityMap;
+
+    private RMapReactive<String, City> cityMap;
 
     public CityService(RedissonReactiveClient client) {
-        this.cityMap = client.getMapCache("city", new TypedJsonJacksonCodec(String.class, City.class));
+        this.cityMap = client.getMap("city", new TypedJsonJacksonCodec(String.class, City.class));
+    }
+
+    public Mono<City> getCity(final String zipCode) {
+        return this.cityMap.get(zipCode)
+                .onErrorResume(ex -> this.cityClient.getCity(zipCode));
+    }
+
+    @Scheduled(fixedRate = 10_000)
+    public void updateCity() {
+        this.cityClient.getAll()
+                .collectList()
+                .map(list -> list.stream().collect(Collectors.toMap(City::getZip, Function.identity())))
+                .flatMap(this.cityMap::putAll)
+                .subscribe();
     }
 
     /*
@@ -30,13 +49,13 @@ public class CityService {
             put it in cache
         return
      */
-    public Mono<City> getCity(final String zipCode) {
-        return this.cityMap.get(zipCode)
-                .switchIfEmpty(this.cityClient.getCity(zipCode)
-                        .flatMap(c -> this.cityMap.fastPut(zipCode, c, 10, TimeUnit.SECONDS)
-                        .thenReturn(c))
-                );
-    }
+//    public Mono<City> getCity(final String zipCode) {
+//        return this.cityMap.get(zipCode)
+//                .switchIfEmpty(this.cityClient.getCity(zipCode)
+//                        .flatMap(c -> this.cityMap.fastPut(zipCode, c, 10, TimeUnit.SECONDS)
+//                        .thenReturn(c))
+//                );
+//    }
 }
 
 
