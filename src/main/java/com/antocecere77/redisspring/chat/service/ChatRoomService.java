07b2f69b7ccac59ab4c753beb53e04aba0c2dad4
antocecere77 <antocecere77@gmail.com>
2021-08-11 07:27:31 +0200

    chat messages broadcasting

--- src/main/java/com/antocecere77/redisspring/chat/service/ChatRoomService.java
+++ src/main/java/com/antocecere77/redisspring/chat/service/ChatRoomService.java
@@ -1,20 +1,53 @@
 package com.antocecere77.redisspring.chat.service;
 
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RTopicReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.client.codec.StringCodec;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class ChatRoomService implements WebSocketHandler {
+
+    private final RedissonReactiveClient client;
 
     @Override
     public Mono<Void> handle(WebSocketSession webSocketSession) {
 
-        //subscribe
-        webSocketSession.receive();
+        String room = "dummy";
+        RTopicReactive topic = this.client.getTopic(room, StringCodec.INSTANCE);
 
-        webSocketSession.send()
-        return null;
+        //subscribe
+        webSocketSession.receive()
+            .doOnNext(WebSocketMessage::getPayloadAsText)
+            .flatMap(topic::publish)
+            .doOnError(System.out::println)
+            .doFinally(s -> System.out.println("subscriber finally " + s))
+            .subscribe();
+
+        //publisher
+        Flux<WebSocketMessage> flux = topic.getMessages(String.class)
+                .map(webSocketSession::textMessage)
+                .doOnError(System.out::println)
+                .doFinally(s -> System.out.println("publisher finally " + s));
+
+        return webSocketSession.send(flux);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
